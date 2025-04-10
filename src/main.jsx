bb5825af79c7c734d2a67a44e6296d62d87072e1
ahmedfathy112 <ahmedfathy241110@gmail.com>
2025-04-10 14:11:50 +0200

    update2

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Portofolio">
+    <HashRouter basename="/Portofolio">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
